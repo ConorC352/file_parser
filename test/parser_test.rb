c563d75a2b0abff0cc1e05bc90e8c658287b5157
ConorC <conorcurran2@hotmail.com>
2021-12-05 09:39:57 +0000

    add assertions to oarser_test.rb

--- test/parser_test.rb
+++ test/parser_test.rb
@@ -31,15 +31,20 @@ class ParserTest < Minitest::Test
   def test_lab_1_txt
     parser = LabResultParser::Parser.new('test/fixtures/lab1.txt')
     assert_equal parser.mapped_results.first.class, LabResultParser::LaboratoryTestResult
+    assert_equal parser.comments_mapped_results.first.class, LabResultParser::LaboratoryTestComment
     assert_equal 'OBX', parser.mapped_results.first.prefix
     assert_equal '1', parser.mapped_results.first.id
     assert_equal 'C100', parser.mapped_results.first.code
     assert_equal '20.0', parser.mapped_results.first.result
+    assert_equal 'NTE', parser.comments_mapped_results[1].prefix
+    assert_equal '1', parser.comments_mapped_results[1].laboratory_test_result_id
+    assert_equal 'Comment for C100 result', parser.comments_mapped_results[1].comment
   end
 
   def test_lab_2_txt
     parser = LabResultParser::Parser.new('test/fixtures/lab2.txt')
     assert_equal parser.mapped_results.first.class, LabResultParser::LaboratoryTestResult
+    assert_equal parser.comments_mapped_results.first.class, LabResultParser::LaboratoryTestComment
     assert_equal 'OBX', parser.mapped_results.first.prefix
     assert_equal '1', parser.mapped_results.first.id
     assert_equal 'A250', parser.mapped_results.first.code
@@ -48,6 +53,9 @@ class ParserTest < Minitest::Test
     assert_equal '2', parser.mapped_results[2].id
     assert_equal 'B250', parser.mapped_results[2].code
     assert_equal '-2.0', parser.mapped_results[2].result
+    assert_equal 'NTE', parser.comments_mapped_results[3].prefix
+    assert_equal '2', parser.comments_mapped_results[3].laboratory_test_result_id
+    assert_equal 'Comment 1 for -2.0 result', parser.comments_mapped_results[3].comment
   end
 
   def test_lab_comments_txt
