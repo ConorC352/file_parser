35b4439ee8cca13c8125f85e466342e02ca7f2e2
ConorC <conorcurran2@hotmail.com>
2021-12-05 05:31:39 +0000

    added test assertions for mapped comments in to lab_results test

--- test/parser_test.rb
+++ test/parser_test.rb
@@ -4,18 +4,28 @@ class ParserTest < Minitest::Test
   def test_lab_results_txt
     parser = LabResultParser::Parser.new('test/fixtures/lab_results.txt')
     assert_equal parser.mapped_results.first.class, LabResultParser::LaboratoryTestResult
+    assert_equal parser.comments_mapped_results.first.class, LabResultParser::LaboratoryTestComment
     assert_equal 'OBX', parser.mapped_results.first.prefix
     assert_equal '1', parser.mapped_results.first.id
     assert_equal 'C100', parser.mapped_results.first.code
     assert_equal '20.0', parser.mapped_results.first.result
+    assert_equal 'NTE', parser.comments_mapped_results[1].prefix
+    assert_equal '1', parser.comments_mapped_results[1].laboratory_test_result_id
+    assert_equal 'Comment for C100 result', parser.comments_mapped_results[1].comment
     assert_equal 'OBX', parser.mapped_results[2].prefix
     assert_equal '2', parser.mapped_results[2].id
     assert_equal 'C200', parser.mapped_results[2].code
     assert_equal '500', parser.mapped_results[2].result
+    assert_equal 'NTE', parser.comments_mapped_results[3].prefix
+    assert_equal '2', parser.comments_mapped_results[3].laboratory_test_result_id
+    assert_equal 'Comment for C200 result', parser.comments_mapped_results[3].comment
     assert_equal 'OBX', parser.mapped_results[6].prefix
     assert_equal '4', parser.mapped_results[6].id
     assert_equal 'B250', parser.mapped_results[6].code
     assert_equal '++', parser.mapped_results[6].result
+    assert_equal 'NTE', parser.comments_mapped_results[8].prefix
+    assert_equal '4', parser.comments_mapped_results[8].laboratory_test_result_id
+    assert_equal 'Comment 2 for ++ result', parser.comments_mapped_results[8].comment
   end
   
   def test_lab_1_txt
